1774f8f6f7917de3583663c3a04ab82bc9f914e4
CLORO <60315633+cloro-official@users.noreply.github.com>
2022-09-06 18:24:25 +0800

    First svelte

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    "./**/*.svelte",
+    "./**/*.html"
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+
+  ],
+}
